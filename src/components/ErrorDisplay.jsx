162541fd86bfc34d5b951b375df477b3a26bea6a
Bradley Showen <BShowen@me.com>
2023-04-23 09:00:00 -0400

    Let ErrorDisplay pickup any thrown route errors automatically

--- src/components/ErrorDisplay.jsx
+++ src/components/ErrorDisplay.jsx
@@ -1,10 +1,16 @@
+import { useRouteError } from "react-router-dom";
 export default function ErrorDisplay({ message }) {
+  const routeError = useRouteError();
+  const errorMessage =
+    message ||
+    routeError?.message ||
+    "Something went wrong. Please go back and try again.";
   return (
     <div className="w-full h-full pt-36 gap-3 flex flex-col justify-start items-center">
       <p className="text-xl">
         <span className="font-semibold">Oops!</span> Something went wrong.
       </p>
-      <p className="text-xl">{message}</p>
+      <p className="text-xl">{errorMessage}</p>
     </div>
   );
 }
