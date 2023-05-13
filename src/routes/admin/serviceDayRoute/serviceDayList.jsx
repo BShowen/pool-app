4ad025dc980ea371f0a9f6b0072665e360d0f505
Bradley Showen <BShowen@me.com>
2023-05-13 13:52:17 -0400

    Use useSorter custom hook for sorting serviceDayList page data

--- src/routes/admin/serviceDayRoute/serviceDayList.jsx
+++ src/routes/admin/serviceDayRoute/serviceDayList.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData, useNavigate } from "react-router-dom";
 import { Disclosure, Transition } from "@headlessui/react";
 import { BsArrowsExpand, BsArrowsCollapse } from "react-icons/bs";
+import useSorter from "../../../hooks/useSorter";
 
 import { getAllServiceRoutes } from "../../../utils/apiFetches";
 import { capitalize, formatAccountName } from "../../../utils/formatters";
@@ -12,7 +13,6 @@ export async function loader() {
 }
 
 export function ServiceDayList() {
-  const navigate = useNavigate();
   const { data, errors, status } = useLoaderData();
 
   if (errors) {
@@ -29,75 +29,112 @@ export function ServiceDayList() {
 
       <div className="w-full pt-16  lg:px-5">
         <div className="mx-auto w-3/4 rounded-2xl bg-white p-3 border-2 gap-5 flex flex-col shadow-sm">
-          {data.routes.map((route) => {
-            const customerAccounts = route.customers;
-            const technician =
-              Object.entries(route.technician).length > 0
-                ? route.technician
-                : { _id: 0, firstName: "Unassigned" };
-
-            return (
-              <Disclosure as="div" key={technician._id} defaultOpen={false}>
-                {({ open }) => (
-                  <>
-                    <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                      <div className="flex flex-row gap-2">
-                        <span className="w-24">
-                          {capitalize(technician.firstName)}
-                        </span>
-                        <span className="badge badge-accent">
-                          {route.count || 0} Customers
-                        </span>
-                      </div>
-                      {open ? (
-                        <BsArrowsCollapse className="text-xl" />
-                      ) : (
-                        <BsArrowsExpand className="text-xl" />
-                      )}
-                    </Disclosure.Button>
-                    <Transition
-                      enter="transition duration-200 ease-out"
-                      enterFrom="transform scale-95 opacity-0"
-                      enterTo="transform scale-100 opacity-100"
-                      leave="transition duration-75 ease-out"
-                      leaveFrom="transform scale-100 opacity-100"
-                      leaveTo="transform scale-95 opacity-0"
-                    >
-                      <Disclosure.Panel className="px-2 text-sm text-gray-500">
-                        <table className="table w-full border-separate border-2 border-t-0 rounded-b-2xl">
-                          <tbody>
-                            {customerAccounts.map((customer) => {
-                              return (
-                                <tr
-                                  draggable
-                                  key={customer._id}
-                                  className="hover:cursor-pointer"
-                                  onClick={() => {
-                                    navigate(
-                                      routes.getDynamicRoute({
-                                        route: "customer",
-                                        id: customer._id,
-                                      })
-                                    );
-                                  }}
-                                >
-                                  <td className="hover:bg-gray-50">
-                                    {formatAccountName(customer.accountName)}
-                                  </td>
-                                </tr>
-                              );
-                            })}
-                          </tbody>
-                        </table>
-                      </Disclosure.Panel>
-                    </Transition>
-                  </>
-                )}
-              </Disclosure>
-            );
-          })}
+          {data.routes.map((route) => (
+            <TechnicianDisclosure
+              key={route.technician._id || 0}
+              route={route}
+            />
+          ))}
         </div>
       </div>
     </div>
+  );
+}
+
+function TechnicianDisclosure({ route }) {
+  const navigate = useNavigate();
+  const [customerAccounts, sortBy] = useSorter(route.customers);
+  const technician =
+    Object.entries(route.technician).length > 0
+      ? route.technician
+      : { _id: 0, firstName: "Unassigned" };
+
+  return (
+    <Disclosure as="div" defaultOpen={false}>
+      {({ open }) => (
+        <>
+          <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+            <div className="flex flex-row gap-2">
+              <span className="w-24">{capitalize(technician.firstName)}</span>
+              <span className="badge badge-accent">
+                {route.count || 0} Customers
+              </span>
+            </div>
+            {open ? (
+              <BsArrowsCollapse className="text-xl" />
+            ) : (
+              <BsArrowsExpand className="text-xl" />
+            )}
+          </Disclosure.Button>
+          <Transition
+            enter="transition duration-200 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
+          >
+            <Disclosure.Panel className="px-2 text-sm text-gray-500">
+              <table className="table w-full border-separate border-spacing-0 border-2 border-t-0 rounded-b-2xl overflow-hidden">
+                <thead>
+                  <tr>
+                    <th>
+                      <span
+                        className="rounded-none hover:cursor-pointer"
+                        onClick={() => sortBy({ category: "accountName" })}
+                        onMouseEnter={(e) =>
+                          e.target.classList.add("text-blue-500")
+                        }
+                        onMouseLeave={(e) =>
+                          e.target.classList.remove("text-blue-500")
+                        }
+                      >
+                        Customer
+                      </span>
+                    </th>
+                    <th>
+                      <span
+                        className="rounded-none hover:cursor-pointer"
+                        onClick={() => sortBy({ category: "serviceDay" })}
+                        onMouseEnter={(e) =>
+                          e.target.classList.add("text-blue-500")
+                        }
+                        onMouseLeave={(e) =>
+                          e.target.classList.remove("text-blue-500")
+                        }
+                      >
+                        Service Day
+                      </span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {customerAccounts.map((customer) => {
+                    return (
+                      <tr
+                        draggable
+                        key={customer._id}
+                        className="hover:cursor-pointer hover"
+                        onClick={() => {
+                          navigate(
+                            routes.getDynamicRoute({
+                              route: "customer",
+                              id: customer._id,
+                            })
+                          );
+                        }}
+                      >
+                        <td>{formatAccountName(customer.accountName)}</td>
+                        <td>{capitalize(customer.serviceDay)}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </Disclosure.Panel>
+          </Transition>
+        </>
+      )}
+    </Disclosure>
   );
 }
