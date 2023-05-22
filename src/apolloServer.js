c1601cf48721cd73d93760a67a4cdc8f046f75b0
Bradley Showen <BShowen@me.com>
2023-05-22 08:06:34 -0400

    Add authorization header to each request

--- src/apolloServer.js
+++ src/apolloServer.js
@@ -1,7 +1,23 @@
 // GraphQL packages.
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-export default new ApolloClient({
+const httpLink = createHttpLink({
   uri: "http://192.168.1.74:8080/",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("apiToken") || "";
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+export default new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
