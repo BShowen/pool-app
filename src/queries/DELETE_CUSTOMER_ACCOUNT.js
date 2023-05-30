5a1e8d35139bf67bbd162484c000eefea9b96f12
Bradley Showen <BShowen@me.com>
2023-05-30 07:55:57 -0400

    Update return value from delete customer query

--- src/queries/DELETE_CUSTOMER_ACCOUNT.js
+++ src/queries/DELETE_CUSTOMER_ACCOUNT.js
@@ -2,6 +2,23 @@ import { gql } from "@apollo/client";
 
 export const DELETE_CUSTOMER_ACCOUNT = gql`
   mutation DeleteCustomerAccount($id: ID) {
-    deleteCustomerAccount(id: $id)
+    deleteCustomerAccount(id: $id) {
+      accountName
+      serviceType
+      serviceDay
+      serviceFrequency
+      address
+      price
+      companyId
+      id
+      technicianId
+      accountOwners {
+        firstName
+        lastName
+        emailAddress
+        phoneNumber
+        id
+      }
+    }
   }
 `;
