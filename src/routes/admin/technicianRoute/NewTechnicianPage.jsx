f1fbfd8483ac5b5f28612d1ce0e41d0eb4c11fdc
Bradley Showen <BShowen@me.com>
2023-05-31 06:52:39 -0400

    Cleanup NewTechnicianPage code

--- src/routes/admin/technicianRoute/NewTechnicianPage.jsx
+++ src/routes/admin/technicianRoute/NewTechnicianPage.jsx
@@ -1,27 +1,5 @@
-import { redirect, useActionData } from "react-router-dom";
-import { useEffect, useState } from "react";
-
 import TechnicianForm from "./technicianComponents/TechnicianForm";
-import LoadingOverlay from "../../../components/LoadingOverlay";
-import { createNewTechnician } from "../../../utils/apiFetches";
 import useInput from "../../../hooks/useInput";
-import routes from "../../routeDefinitions";
-
-// async function action({ request }) {
-//   const formData = await request.formData();
-//   const formObject = Object.fromEntries(formData);
-//   const { protocol, host } = window.location;
-//   // This is the url that brings the user from their email to the app.
-//   formObject.registrationUrl = `${protocol}//${host}${routes.registerTechnician}`;
-//   const { status, data, errors } = await createNewTechnician(formObject);
-//   if (status === 201) {
-//     return redirect(
-//       routes.getDynamicRoute({ route: "technician", id: data.technician.id })
-//     );
-//   } else {
-//     return errors;
-//   }
-// }
 
 export default function NewTechnicianPage() {
   const [firstName] = useInput({
@@ -57,29 +35,11 @@ export default function NewTechnicianPage() {
     },
   });
 
-  // useEffect(() => {
-  //   if (errors.emailAddress) {
-  //     setEmailError(errors.emailAddress);
-  //   }
-  //   // if (errors.message) {
-  //   //   // Render the alert for three seconds
-  //   //   setAlertVisibility(true);
-  //   //   setTimeout(() => {
-  //   //     setAlertVisibility(false);
-  //   //   }, 3000);
-  //   // }
-  // }, [errors]);
-
   return (
-    <>
-      {/* LoadingOverlay is conditionally rendered. Only when submitting. */}
-      <LoadingOverlay />
-      <TechnicianForm
-        inputs={[firstName, lastName, email]}
-        title={"New technician"}
-        onError={setEmailError}
-      />
-    </>
+    <TechnicianForm
+      inputs={[firstName, lastName, email]}
+      title={"New technician"}
+      onError={setEmailError}
+    />
   );
-  // }
 }
