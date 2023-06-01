67feb0735004e1f3adc138d4cb75315ed26ea8ef
Bradley Showen <BShowen@me.com>
2023-06-01 07:08:01 -0400

    Implement NewTechnicianPag with refactored technician form component

--- src/routes/admin/technicianRoute/NewTechnicianPage.jsx
+++ src/routes/admin/technicianRoute/NewTechnicianPage.jsx
@@ -1,7 +1,19 @@
-import TechnicianForm from "./technicianComponents/TechnicianForm";
+import { useMutation } from "@apollo/client";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { TechnicianForm } from "./technicianComponents/TechnicianForm";
+import { CREATE_TECHNICIAN, TECHNICIAN_LIST } from "../../../queries/index.js";
 import useInput from "../../../hooks/useInput";
+import routes from "../../routeDefinitions";
+import Loading from "../../../components/Loading";
 
 export default function NewTechnicianPage() {
+  const navigate = useNavigate();
+  const [createTechnician, { loading, data }] = useMutation(CREATE_TECHNICIAN, {
+    refetchQueries: [{ query: TECHNICIAN_LIST }],
+  });
+
   const [firstName] = useInput({
     value: "",
     type: "text",
@@ -21,7 +33,7 @@ export default function NewTechnicianPage() {
     error: "Last name is required",
   });
 
-  const [email, setEmailError] = useInput({
+  const [emailAddress] = useInput({
     value: "",
     type: "email",
     name: "emailAddress",
@@ -35,11 +47,53 @@ export default function NewTechnicianPage() {
     },
   });
 
+  const formFields = { firstName, lastName, emailAddress };
+
+  async function handleSubmit({ formData }) {
+    const isValid = !Object.values(formFields)
+      .map((input) => input.validate())
+      .includes(false);
+    if (!isValid) return;
+    const { protocol, host } = window.location;
+    const variables = { technician: { ...formData } };
+    // ---------------------------------------------------------------
+    // This is the url that brings the user from their email to the app.
+    // This string is not persisted in the DB. It is used in the backend
+    // and data is appended to this string before an email is sent to the
+    // technician with this url as a link within the email.
+    variables.technician.registrationUrl = `${protocol}//${host}${routes.registerTechnician}`;
+    // ---------------------------------------------------------------
+    try {
+      await createTechnician({ variables });
+    } catch (error) {
+      const errorObj = error.graphQLErrors[0].extensions.fields;
+      for (const [key, value] of Object.entries(errorObj)) {
+        // key is the formField. i.e firstName, emailAddress, etc.
+        // value is the error message for that field.
+        formFields[key].dispatch({ action: "SET_ERROR", value });
+      }
+    }
+  }
+
+  useEffect(() => {
+    // Redirect to the technician page after a successful submission.
+    if (data) {
+      const technicianId = data.createNewTechnician.id;
+      navigate(
+        routes.getDynamicRoute({ route: "technician", id: technicianId })
+      );
+    }
+  }, [data]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <TechnicianForm
-      inputs={[firstName, lastName, email]}
-      title={"New technician"}
-      onError={setEmailError}
+      inputList={Object.values(formFields)}
+      formTitle={"New technician"}
+      onSubmit={handleSubmit}
     />
   );
 }
