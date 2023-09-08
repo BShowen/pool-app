989349dcecb77245e271d59879c7f70d34b5a814
Bradley Showen <BShowen@me.com>
2023-09-08 07:53:54 -0400

    Improve MobileBottomNav UI

--- src/routes/admin/adminComponents/MobileBottomNav.jsx
+++ src/routes/admin/adminComponents/MobileBottomNav.jsx
@@ -1,75 +1,30 @@
 import { Link } from "react-router-dom";
-
+import { FaRoute, FaHome } from "react-icons/fa";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 export default function MobileBottomNav({ isSidebarOpen, toggleSidebar }) {
   return (
-    <div className="btm-nav btm-nav-lg lg:hidden bg-slate-100 z-50">
-      {/* <div className="btm-nav btm-nav-lg lg:hidden h-28"> */}
+    <div className="btm-nav btm-nav-lg lg:hidden bg-slate-100 z-50 h-12">
       <Link as="button" to="/">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-          />
-        </svg>
+        <FaHome className="text-xl" />
       </Link>
-      <button>
-        {/* <button className="active"> */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-      </button>
-      <div>
-        <label className="swap swap-rotate" onChange={toggleSidebar}>
-          {/* <!-- this hidden checkbox controls the state --> */}
-          <input
-            type="checkbox"
-            className="invisible"
-            checked={isSidebarOpen}
-            readOnly
-          />
+      <Link as="button" to="/serviceRoute">
+        <FaRoute className="text-xl" />
+      </Link>
+      <label className="swap swap-rotate" onChange={toggleSidebar}>
+        {/* <!-- this hidden checkbox controls the state --> */}
+        <input
+          type="checkbox"
+          className="invisible"
+          checked={isSidebarOpen}
+          readOnly
+        />
 
-          {/* <!-- hamburger icon --> */}
-          <svg
-            className="swap-off fill-current"
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 512 512"
-          >
-            <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
-          </svg>
+        {/* <!-- hamburger icon --> */}
+        <AiOutlineMenu className="text-2xl swap-off" />
 
-          {/* <!-- close icon --> */}
-          <svg
-            className="swap-on fill-current"
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 512 512"
-          >
-            <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
-          </svg>
-        </label>
-      </div>
+        {/* <!-- close icon --> */}
+        <AiOutlineClose className="text-2xl swap-on" />
+      </label>
     </div>
   );
 }
